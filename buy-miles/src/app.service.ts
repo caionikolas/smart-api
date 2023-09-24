3eee5687ad894a7936e10a284be6f750d42a482e
caionikolas <Caio.kanu@hotmail.com>
2023-09-24 10:43:39 -0300

    test-miles

--- buy-miles/src/app.service.ts
+++ buy-miles/src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { milesDto } from './dtos/miles.dto';
 
 const calcMiles = {
@@ -16,15 +16,41 @@ const calcMiles = {
 @Injectable()
 export class AppService {
   postMiles(miles: milesDto) {
+    if (
+      miles.quantMiles % 1000 != 0 ||
+      miles.desconto < 0 ||
+      miles.desconto > 80 ||
+      miles.bonus < 0 ||
+      miles.bonus > 300
+    ) {
+      throw new BadRequestException('Something bad happened ', {
+        cause: new Error(),
+        description: 'Valores de dados invalidos!',
+      });
+    }
+
     calcMiles.quantMiles = miles.quantMiles;
     calcMiles.desconto = miles.desconto;
     calcMiles.valor_desconto = 70 * (1 - miles.desconto / 100);
     calcMiles.bonus = miles.bonus;
-    calcMiles.milhasBonus = miles.quantMiles;
+    calcMiles.milhasBonus = miles.quantMiles * (miles.bonus / 100);
+    calcMiles.milhasTotais = miles.quantMiles + calcMiles.milhasBonus;
+    calcMiles.valorTotal = calcMiles.valor_desconto;
+    calcMiles.valorFinal =
+      calcMiles.valorTotal / (calcMiles.milhasTotais / 1000);
 
     return {
-      valorRef: `R$ ${calcMiles.valorRef},00/milheiro`,
-      milhasComprar: `R$ ${calcMiles.milhasBonus},00/ilheiro`,
+      'valor Ref.': `R$ ${calcMiles.valorRef.toFixed(2)}/milheiro`,
+      'Milhas Comprar': calcMiles.quantMiles,
+      Desconto: `${calcMiles.desconto}%`,
+      'Valor com desc.': `R$ ${calcMiles.valor_desconto.toFixed(2)}/milheiro`,
+      Bônus: `${calcMiles.bonus}%`,
+      'Milhas de Bônus': calcMiles.milhasBonus,
+      'Milhas Totais': calcMiles.milhasTotais,
+      'Valor Total': `R$ ${calcMiles.valorTotal.toFixed(2)}`,
+      'VALOR FINAL MILHEIRO': `>> R$ ${calcMiles.valorFinal.toFixed(2)} <<`,
+      'Classificação da Compra': 'Excelente :)',
+      Reconmendação: 'Pode Comprar',
     };
   }
 }
